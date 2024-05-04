24dab6e88b7626a398f4cfa1280b484b069b6a51
Jasurbek <jasurkamilov7279@gmail.com>
2024-05-04 23:17:18 +0500

    .

--- main/views.py
+++ main/views.py
@@ -84,7 +84,7 @@ def callback_query(call):
         keyboard = types.InlineKeyboardMarkup(row_width=2)
         keyboard.add(types.InlineKeyboardButton(text="Ko'proq ma'lumot olish", url="https://rashidevs.uz"))
         keyboard.add(types.InlineKeyboardButton(text='🔙 Ortga', callback_data="home_page_back"))
-        bot.send_photo(call.message.chat.id, photo='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPB7w3Z7LmbiOsNTBfKZQpK_sOv7RXRbmv2YB6EH2cMw&s', caption='''
+        bot.send_photo(call.message.chat.id, photo='https://rashidevs.pyblog.uz/media/rashidevs_bot_baner.png', caption='''
 Rashidevs.uz – dasturlash va zamonaviy AyTi kasblarini o'qituvchi onlayn kurslar platformasi🔥
 
 Mohirdev – bu shunchaki ta’lim emas, bu o’zgarish ishtiyoqida yonayotgan yosh yuraklar uchun bir mayoqdir.💡
